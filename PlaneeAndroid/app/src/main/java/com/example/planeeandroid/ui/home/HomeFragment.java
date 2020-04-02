924d3748e2035df1131b2098be4b5c6501bb3232
Benjamin <adolphe906@gmail.com>
2020-04-02 22:46:02 +0400

    Ajout du fragment pour l'ajout d'évènement

--- PlaneeAndroid/app/src/main/java/com/example/planeeandroid/ui/home/HomeFragment.java
+++ PlaneeAndroid/app/src/main/java/com/example/planeeandroid/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.planeeandroid.ui.home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.UrlQuerySanitizer;
 import android.os.Bundle;
@@ -16,13 +17,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.planeeandroid.Evenement;
+import com.example.planeeandroid.MainActivity;
 import com.example.planeeandroid.MyArrayAdapter;
 import com.example.planeeandroid.R;
 import com.example.planeeandroid.Tache;
+import com.example.planeeandroid.ui.add.addFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -39,7 +43,8 @@ public class HomeFragment extends Fragment {
     Tache tache = new Tache("Tache 1", "Magasin", "url");
     ArrayList<Tache> taches = new ArrayList<Tache>();
     Evenement evenement = new Evenement("Test", today, taches);
-    Evenement[] evenements = {evenement};
+    Evenement test = new Evenement("Test2", today, taches);
+    private Evenement[] evenements = {evenement, test};
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,35 +55,16 @@ public class HomeFragment extends Fragment {
         ListView maListeView = root.findViewById(R.id.List);
         MyArrayAdapter myArray = new MyArrayAdapter(root.getContext(), evenements);
         maListeView.setAdapter(myArray);
-      /*  text = root.findViewById(R.id.TextTest);
-        bouton = root.findViewById(R.id.Button);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        flag = true;
-        View.OnClickListener Texte = new View.OnClickListener() {
+        FloatingActionButton plus = root.findViewById(R.id.Plus);
+        plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (flag) {
-                    text.setText("Ceci est un test");
-                } else {
-                    text.setText("");
-                }
-                flag = !flag;
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.HomeFragment, new addFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
             }
-        };
-        bouton.setOnClickListener(Texte);*/
+        });
         return root;
     }
-
-    /*public void CreatCard(ArrayList<Evenement> Evenements, LinearLayout Linear, Context context) {
-        for (int i = 1; i <= Evenements.size(); i++) {
-            ConstraintLayout constraint = new ConstraintLayout(context);
-            Button bouton = new Button(context);
-            Linear.addView(constraint);
-        }
-    }*/
 }
