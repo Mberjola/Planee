18c27d1f1a7c4c8f279a6a8bb0f8ef4433be8cea
Benjamin <adolphe906@gmail.com>
2020-04-06 09:10:27 +0400

    Calendrier Fixed

--- PlaneeAndroid/app/src/main/java/com/example/planeeandroid/ui/add/addFragment.java
+++ PlaneeAndroid/app/src/main/java/com/example/planeeandroid/ui/add/addFragment.java
@@ -80,7 +80,7 @@ public class addFragment extends Fragment {
                 int year = cal.get(Calendar.YEAR);
                 int month = cal.get(Calendar.MONTH);
                 int day = cal.get(Calendar.DAY_OF_MONTH);
-                DatePickerDialog dialog = new DatePickerDialog(root.getContext(), android.R.style.Holo_Light_ButtonBar, myDateSetListener, year, month, day);
+                DatePickerDialog dialog = new DatePickerDialog(root.getContext(), android.R.style.Theme_Holo_Light_Dialog, myDateSetListener, year, month, day);
                 dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
                 dialog.show();
             }
